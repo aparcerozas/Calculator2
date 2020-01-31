aaab22ad74bc08e7c1488145954c53753486efc6
servidor <servidor@192.168.0.1>
2020-01-31 10:01:44 +0100

    Multicliente

--- src/calculator2/Calculator2.java
+++ src/calculator2/Calculator2.java
@@ -7,80 +7,105 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 
-public class Calculator2 {
-    public static void main(String[] args) {
+public class Calculator2 extends Thread {
+    
+    private Socket clientSocket;
+
+    public Calculator2(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+    
+    @Override
+    public void run() {
         try{
-            System.out.println("Creando socket servidor");
-            ServerSocket serverSocket = new ServerSocket();
-            System.out.println("Realizando el bind");
-            InetSocketAddress addr = new InetSocketAddress("192.168.0.1", 5555);
-            serverSocket.bind(addr);
-            System.out.println("Aceptando conexiones");
-            Socket newSocket = serverSocket.accept();
-            System.out.println("Conexion recibida");
+            InputStream is = clientSocket.getInputStream();
+            OutputStream os = clientSocket.getOutputStream();
             
-            InputStream is = newSocket.getInputStream();
-            OutputStream os = newSocket.getOutputStream();
+            System.out.println("Preguntando por primer operando");
+            String envio = "Envíe el primer operando";
+            os.write(envio.getBytes());
             
-            System.out.println("Preguntando por tipo de operación");
-            String pregunta = "Para hacer una suma, pulse 1;"
-                    + " para hacer una resta, pulse 2; "
-                    + "para hacer una multiplicación, pulse 3;"
-                    + " y para hacer una división, pulse 4";
-            os.write(pregunta.getBytes());
+            System.out.println("Recibiendo primer operando");
+            byte[] mensaje = new byte[40];
+            is.read(mensaje);
+            double num1 = Double.parseDouble(new String(mensaje));
             
-            byte[] mensaje = new byte[25];
-            byte[] operand = new byte[1];
-            System.out.println("Recibiendo respuesta de operación");
-            is.read(operand);
+            System.out.println("Preguntando por segundo operando");
+            envio = "Envíe el segundo operando";
+            os.write(envio.getBytes());
             
-            String operando = new String(operand);
-            String respuesta;
-            switch(operando){
-                case "1":
-                    System.out.println("Suma solicitada");
-                    System.out.println("Preguntando por primer operando");
-                    respuesta = "Operación aceptada. Envíe el primer operando";
-                    os.write(respuesta.getBytes());
-                    
-                    is.read(mensaje);
-                    double num1 = Double.parseDouble(new String(mensaje));
-                    
-                    System.out.println("Preguntando por segundo operando");
-                    respuesta = "Envíe el segundo operando";
-                    os.write(respuesta.getBytes());
-                    
-                    is.read(mensaje);
-                    double num2 = Double.parseDouble(new String(mensaje));
-                    
-                    System.out.println("Calculando operación");
-                    double operacion = num1 + num2;
-                    System.out.println("Enviando resultado de operación");
-                    respuesta = Double.toString(operacion);
-                    os.write(respuesta.getBytes());
-                case "2":
-                    System.out.println("Resta solicitada");
-                case "3":
-                    System.out.println("Multiplicación solicitada");
-                case "4":
-                    System.out.println("División solicitada");
-                default:
-                    System.out.println("Operando del cliente incorrecto");
-                    respuesta = "Error. Solicitando desconexión";
-                    os.write(respuesta.getBytes());
+            System.out.println("Recibiendo segundo operando");
+            mensaje = new byte[25];
+            is.read(mensaje);
+            double num2 = Double.parseDouble(new String(mensaje));
+            
+            System.out.println("Preguntando por el tipo de operación");
+            envio = "Envíe el tipo de operación: 1=suma, 2=resta, 3=mult, 4=div";
+            os.write(envio.getBytes());
+            
+            System.out.println("Recibiendo tipo de operación");
+            mensaje = new byte[25];
+            is.read(mensaje);
+            double operacion = Double.parseDouble(new String(mensaje));
+            
+            double resultado;
+            if (operacion == 1.0){
+                resultado = num1 + num2;
+                envio = num1+" + "+num2+" = "+resultado;
+                os.write(envio.getBytes());
             }
-                    
+            else if (operacion == 2.0){
+                resultado = num1 - num2;
+                envio = num1+" - "+num2+" = "+resultado;
+                os.write(envio.getBytes());
+            }
+            else if (operacion == 3.0){
+                resultado = num1 * num2;
+                envio = num1+" * "+num2+" = "+resultado;
+                os.write(envio.getBytes());
+            }
+            else if (operacion == 4.0){
+                resultado = num1 / num2;
+                envio = num1+" / "+num2+" = "+resultado;
+                os.write(envio.getBytes());
+            }
+            else {
+                envio = "Error. Operación incorrecta";
+                os.write(envio.getBytes());
+            }
             
-            System.out.println("Cerrando el nuevo socket");
-            newSocket.close();
-            
-            System.out.println("Cerrando el socket servidor");
-            serverSocket.close();
-            
-            System.out.println("Terminado");
         } catch (IOException e){
             e.printStackTrace();
         }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Creando socket servidor");
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket();
+            System.out.println("Realizando el bind");
+            InetSocketAddress addr = new InetSocketAddress("192.168.0.1", 5555);
+            serverSocket.bind(addr);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        
+        System.out.println("Aceptando conexiones");
+        
+        while (serverSocket != null) {
+            try {
+                Socket newSocket = serverSocket.accept();
+                System.out.println("Conexión recibida");
+                
+                Calculator2 hilo = new Calculator2(newSocket);
+                hilo.start();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        
+        System.out.println("Terminado");
     }
     
 }
